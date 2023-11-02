5681297079b7b75e6e16493873df5d3c63f3d5d0
sarkh200 <sarkh200@gmail.com>
2023-11-02 19:45:35 -0400

    Moved forge configs from package.json

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,37 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+  packagerConfig: {},
+      makers: [
+        {
+          name: "@electron-forge/maker-squirrel",
+          config: {
+            name: "electron_quick_start"
+          }
+        },
+        {
+          name: "@electron-forge/maker-zip",
+          platforms: [
+            darwin
+          ]
+        },
+        {
+          name: "@electron-forge/maker-deb",
+          config: {}
+        },
+        {
+          name: "@electron-forge/maker-rpm",
+          config: {}
+        }
+      ],
+      publishers: [
+        {
+          name: "@electron-forge/publisher-github",
+          config: {
+            repository: {
+              owner: "sarkh200",
+              name: "iranian-tv-electron"
+            },
+            draft: true
+          }
+        }
+      ]
 };
